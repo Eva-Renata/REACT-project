01d4979dafacd6216a5f0de85f7070bafa0f6497
Eva-Renata <all2evi@yahoo.com>
2022-01-13 16:48:33 +0100

    logo png tilføjet

--- CreateReactApp/myapp/src/components/partials/Header/Header.js
+++ CreateReactApp/myapp/src/components/partials/Header/Header.js
@@ -1,8 +1,10 @@
 import './Header.scss'
+import logo from '../../../assets/images/icons8-react-native-100.png'
 
 export const Header = (props) => {
   return (
     <header>
+      <img src={logo}></img>
       <h1>{props.title}</h1>
     </header>
 
